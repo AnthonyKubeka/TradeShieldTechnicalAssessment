7265cb615a1bde112606baecc940b2aba28306b2
= <nmkubeka@gmail.com>
2024-06-30 20:56:58 +0200

    extract text into sentences

--- TS.TechnicalTest/LongestSentanceAnswer.cs
+++ TS.TechnicalTest/LongestSentanceAnswer.cs
@@ -5,6 +5,23 @@ public class LongestSentanceAnswer
 {
     public static int Solution(string s)
     {
-        throw new NotImplementedException("Not completed yet");
+        int answer = 0;
+        var sentences = new List<string>();
+        var sentenceDelimeters = new char[]
+        {
+            '?',
+            '.',
+            '!'
+        };
+
+        for (int i = 0; i < 100; i++)
+        {
+            var sentenceIndex = s.IndexOfAny(sentenceDelimeters);
+            var sentence = s.Substring(0, sentenceIndex + 1);
+            sentences.Add(sentence);
+            s = s.Remove(0, sentenceIndex + 1);
+        }
+
+        return answer; 
     }
 }
